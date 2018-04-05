24d6c0626fa57eb067d246b5ae5993f3f976c56d
Diego Morales <diego.morales@gmx.net>
2018-04-05 15:21:35 +0200

    add stylelint standard, remove sass, add postcss plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 let gulp = require('gulp'),
-    path = require('path'),
-    browser = require('browser-sync'),
-    del = require('del'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('autoprefixer'),
-    postcss = require('gulp-postcss'),
-    cssnano = require('cssnano'),
-    stylelint = require('gulp-stylelint'),
-    sourcemaps = require('gulp-sourcemaps'),
-    modernizr = require('gulp-modernizr'),
-    uglify = require('gulp-uglify'),
-    webpack = require('webpack'),
-    UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
-    eslint = require('gulp-eslint');
+  path = require('path'),
+  browser = require('browser-sync'),
+  del = require('del'),
+  postcss = require('gulp-postcss'),
+  cssnext = require('postcss-cssnext'),
+  atImport = require('postcss-import'),
+  precss = require('precss'),
+  cssnano = require('cssnano'),
+  stylelint = require('gulp-stylelint'),
+  sourcemaps = require('gulp-sourcemaps'),
+  modernizr = require('gulp-modernizr'),
+  uglify = require('gulp-uglify'),
+  webpack = require('webpack'),
+  UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
+  eslint = require('gulp-eslint');
 
 // Set default environment
 process.env.NODE_ENV = 'development';
@@ -20,214 +21,214 @@ process.env.NODE_ENV = 'development';
 const isProd = () => process.env.NODE_ENV === 'production';
 
 const paths = {
-    dev: './src/',
-    get devPages() {
-        return this.dev + 'pages/';
-    },
-    get devScss() {
-        return this.dev + 'scss/';
-    },
-    get devJs() {
-        return this.dev + 'js/';
-    },
-    get devAssets() {
-        return this.dev + 'assets/';
-    },
-    build: './build/',
-    get buildCss() {
-        return this.build + 'css/';
-    },
-    get buildJs() {
-        return this.build + 'js/';
-    },
-    get buildAssets() {
-        return this.build + 'assets/';
-    }
+  dev: './src/',
+  get devPages() {
+    return this.dev + 'pages/';
+  },
+  get devCss() {
+    return this.dev + 'css/';
+  },
+  get devJs() {
+    return this.dev + 'js/';
+  },
+  get devAssets() {
+    return this.dev + 'assets/';
+  },
+  build: './build/',
+  get buildCss() {
+    return this.build + 'css/';
+  },
+  get buildJs() {
+    return this.build + 'js/';
+  },
+  get buildAssets() {
+    return this.build + 'assets/';
+  }
 };
 
 const startServer = () => browser.init({
-    server: {
-        baseDir: './build',
-        port: 3000
-    },
-    open: false
+  server: {
+    baseDir: './build',
+    port: 3000
+  },
+  open: false
 });
 
 const reload = (done) => {
-    browser.reload();
-    done();
+  browser.reload();
+  done();
 };
 
 const cleanBuild = () => del(paths.build);
 
 const copyPages = () => {
-    return gulp.src(paths.devPages + '*.html')
-        .pipe(gulp.dest(paths.build));
+  return gulp.src(paths.devPages + '*.html')
+    .pipe(gulp.dest(paths.build));
 };
 
-const buildSass = () => {
-    let postCssTasks = [
-        autoprefixer({browsers: ['last 2 versions']})
-    ];
+const buildCss = () => {
+  let postCssTasks = [
+    atImport(),
+    precss(),
+    cssnext({browsers: ['last 2 versions']})
+  ];
 
-    if (isProd()) {
-        postCssTasks.push(cssnano({
-            discardComments: {
-                removeAll: true
-            }
-        }));
-    }
+  if (isProd()) {
+    postCssTasks.push(cssnano({
+      discardComments: {
+        removeAll: true
+      }
+    }));
+  }
 
-    return gulp.src(paths.devScss + '**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass()
-            .on('error', sass.logError))
-        .pipe(postcss(postCssTasks))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.buildCss))
+  return gulp.src(paths.devCss + '**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss(postCssTasks))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.buildCss))
 
-        // reload browser
-        .pipe(browser.stream({match: '**/*.css'}));
+    // reload browser
+    .pipe(browser.stream({match: '**/*.css'}));
 };
 
-const lintSass = () => {
-    return gulp.src([
-        paths.devScss + '**/*.scss'
-    ])
-        .pipe(stylelint({
-            failAfterError: false,
-            reporters: [
-                {
-                    formatter: 'string',
-                    console: true
-                }
-            ]
-        }));
+const lintCss = () => {
+  return gulp.src([
+    paths.devCss + '**/*.css'
+  ])
+    .pipe(stylelint({
+      failAfterError: false,
+      reporters: [
+        {
+          formatter: 'string',
+          console: true
+        }
+      ]
+    }));
 };
 
 const buildJs = (done) => {
-    let plugins = [];
+  let plugins = [];
 
-    if (isProd()) {
-        plugins.push(new UglifyJsPlugin({
-            sourceMap: true
-        }));
+  if (isProd()) {
+    plugins.push(new UglifyJsPlugin({
+      sourceMap: true
+    }));
+  }
+
+  webpack({
+    watch: !isProd(),
+    devtool: isProd() ? 'source-map' : 'eval-source-map',
+    entry: {
+      main: path.resolve(paths.devJs, 'main.js')
+    },
+    output: {
+      publicPath: '/',
+      filename: '[name].js',
+      path: path.resolve(__dirname, paths.buildJs)
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['env']
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: plugins
+  }, (err, stats) => {
+    if (err) {
+      console.error(err.stack || err);
+      if (err.details) {
+        console.error(err.details);
+      }
+      return;
     }
 
-    webpack({
-        watch: !isProd(),
-        devtool: isProd() ? 'source-map' : 'eval-source-map',
-        entry: {
-            main: path.resolve(paths.devJs, 'main.js')
-        },
-        output: {
-            publicPath: '/',
-            filename: '[name].js',
-            path: path.resolve(__dirname, paths.buildJs)
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /(node_modules)/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: ['env']
-                            }
-                        }
-                    ]
-                }
-            ]
-        },
-        plugins: plugins
-    }, (err, stats) => {
-        if (err) {
-            console.error(err.stack || err);
-            if (err.details) {
-                console.error(err.details);
-            }
-            return;
-        }
+    const info = stats.toJson();
 
-        const info = stats.toJson();
+    if (stats.hasErrors()) {
+      console.error(info.errors);
+    }
 
-        if (stats.hasErrors()) {
-            console.error(info.errors);
-        }
+    if (stats.hasWarnings()) {
+      console.warn(info.warnings);
+    }
 
-        if (stats.hasWarnings()) {
-            console.warn(info.warnings);
-        }
+    // Log result...
+    console.log(stats.toString({
+      chunks: false,  // Makes the build much quieter
+      modules: false,
+      colors: true,    // Shows colors in the console
+      moduleTrace: false
+    }));
 
-        // Log result...
-        console.log(stats.toString({
-            chunks: false,  // Makes the build much quieter
-            modules: false,
-            colors: true,    // Shows colors in the console
-            moduleTrace: false
-        }));
-
-        browser.reload();
-        done();
-    });
+    browser.reload();
+    done();
+  });
 };
 
 const lintJs = () => {
-    return gulp.src([
-        paths.devJs + '**/*.js'
-    ])
-        .pipe(eslint())
-        .pipe(eslint.format());
+  return gulp.src([
+    paths.devJs + '**/*.js'
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format());
 };
 
 const copyAssets = () => {
-    return gulp.src(paths.devAssets + '**/*.*')
-        .pipe(gulp.dest(paths.buildAssets));
+  return gulp.src(paths.devAssets + '**/*.*')
+    .pipe(gulp.dest(paths.buildAssets));
 };
 
 const buildModernizr = () => {
-    return gulp.src([paths.devJs + '**/*.js', paths.devScss + '**/*.scss'])
-        .pipe(modernizr('modernizr-custom.js', {
-            options: [
-                'setClasses',
-                'addTest',
-                'html5printshiv',
-                'testProp',
-                'fnBind'
-            ],
-            excludeTests: ['hidden']
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.buildJs));
+  return gulp.src([paths.devJs + '**/*.js', paths.devCss + '**/*.css'])
+    .pipe(modernizr('modernizr-custom.js', {
+      options: [
+        'setClasses',
+        'addTest',
+        'html5printshiv',
+        'testProp',
+        'fnBind'
+      ],
+      excludeTests: ['hidden']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.buildJs));
 };
 
 const build = gulp.series(cleanBuild, gulp.parallel(
-    copyAssets,
-    copyPages,
-    buildModernizr,
-    buildSass,
-    buildJs,
-    lintSass,
-    lintJs
+  copyAssets,
+  copyPages,
+  buildModernizr,
+  buildCss,
+  buildJs,
+  lintCss,
+  lintJs
 ));
 
 const watch = gulp.series(build, () => {
-    startServer();
+  startServer();
 
-    gulp.watch([paths.devJs + '**/*.js'], lintJs);
-    gulp.watch([paths.devPages + '*.html'], gulp.series(copyPages, reload));
-    gulp.watch([paths.devScss + '**/*.scss'], gulp.parallel(buildSass, lintSass));
-    gulp.watch([paths.devAssets + '**/*.*'], gulp.series(copyAssets, reload));
+  gulp.watch([paths.devJs + '**/*.js'], lintJs);
+  gulp.watch([paths.devPages + '*.html'], gulp.series(copyPages, reload));
+  gulp.watch([paths.devCss + '**/*.css'], gulp.parallel(buildCss, lintCss));
+  gulp.watch([paths.devAssets + '**/*.*'], gulp.series(copyAssets, reload));
 });
 
 gulp.task('default', watch);
 
 gulp.task('build', gulp.series((done) => {
 
-    // Set environment
-    process.env.NODE_ENV = 'production';
+  // Set environment
+  process.env.NODE_ENV = 'production';
 
-    done();
+  done();
 }, build));
