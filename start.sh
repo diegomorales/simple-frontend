a74daffb63ed8f9f309b16ff3b32b2fce051bd0b
Diego Morales <diego.morales@gmx.net>
2018-10-09 23:32:24 +0200

    remove nfs stuff again

--- start.sh
+++ start.sh
@@ -6,14 +6,4 @@ if [[ "$(docker images -q $IMG_NAME 2> /dev/null)" == "" ]]; then
   docker build -t $IMG_NAME .
 fi
 
-if [[ "$(pgrep nfsd 2> /dev/null)" != "" ]]; then
-	docker run \
-	-it \
-	-p 3000:3000 -p 3001:3001 \
-	--mount 'type=volume,volume-driver=local,"volume-opt=o=addr=host.docker.internal,rw,nolock,hard,nointr,nfsvers=3",volume-opt=type=nfs,volume-opt=device=:'"$(pwd)"',dst=/home/node/code,src='"$IMG_NAME"'_volume' \
-	$IMG_NAME bash
-else
-	docker run -it -p 3000:3000 -p 3001:3001 -v $(pwd):/home/node/code $IMG_NAME bash
-fi
-
-
+docker run -it -p 3000:3000 -p 3001:3001 -v $(pwd):/home/node/code $IMG_NAME bash
